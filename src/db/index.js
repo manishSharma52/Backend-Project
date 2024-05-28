ea3d6fb6cc13e1266306b0c0d573d20362a2bc88
Your Name <you@example.com>
2024-05-28 17:02:04 +0530

    connect mongo atlas

--- src/db/index.js
+++ src/db/index.js
@@ -16,3 +16,33 @@ const connectDB = async () => {
 
 
 export default connectDB
+
+
+
+
+
+
+
+
+
+import express from "express"
+// const app = express()
+
+// ;(async ()=>{
+//     try {
+//        await mongoose.connect(`${process.env.
+//         MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", ()=>{
+//             console.log("ERR:", error)
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listen on port 
+//             ${process.env.PORT}` )
+//         })
+        
+//     } catch (error) {
+//         console.error("ERROR:",error)
+//     }
+// })()
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 // require ('dotenv').config({path: './env'})
 import dotenv from "dotenv"
+import mongoose from "mongoose";
+
+import { DB_NAME } from "../constants";
+
 import connectDB from "./db/index.js";
 
 dotenv.config({
@@ -9,7 +13,15 @@ dotenv.config({
 
 connectDB()
 
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () =>{
+        console.log(`server is running at port :
+         ${process.env.PORT}`);
+    })
+})
+.catch((err)=> {
+    console.log("MONGO db connection failed !!!", err)
+})
 
 
 
