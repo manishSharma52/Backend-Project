545a3dbb86aec06c96cedafeedd7c616e3529426
Your Name <you@example.com>
2024-05-31 12:58:00 +0530

    cloudinary and multer discussion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+
+
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY , 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View Credentials' below to copy your API secret
+    });
+
+        const uploadOnCloudinary = async(localFilePath) => {
+            try {
+
+                if (!localFilePath) return null 
+                  const response = await cloudinary.uploader.upload(localFilePath,{
+                    resource_type: "auto"
+                   }) 
+
+                   console.log("file is uploaded on cloudinary",
+                    response.url);
+                    return response;
+            } catch (error) {
+
+            fn.unlinkSync(localFilePath)
+            return null
+            }
+        }
+   
+
+  export {uploadOnCloudinary}
+
+   
+   
+   
+//     cloudinary.uploader.upload("https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg", {
+//         public_id: "shoes"
+//     }, 
+//         function (error,result)
+//         {console.log(result);}
+// );
